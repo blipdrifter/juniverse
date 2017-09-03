79f1c7e9ac2d0eea1449f0fa5ee608b61ea31280
Spriteclad <spriteclad@users.noreply.github.com>
2017-09-03 16:56:25 -0700

    Changed a bunch of stuff

--- header.js
+++ header.js
@@ -1,5 +1,5 @@
 document.write('<div id="header">');
-document.write('<a href="index.html" ><img class="header" src="img/logo.png" alt="Across the Juniverse" /></a>');
+document.write('<img class="header" src="img/logo.png" alt="Across the Juniverse" />');
 document.write('<div id="nav">');
 document.write('<p><a href="index.html" >home</a> | <a href="games.html" >games</a> | <a href="mods.html" >mods</a> | <a href="levels.html">levels</a> | <a href="resources.html" >resources</a> | <a href="about.html" >about</a> | <a href="links.html">links</a>');
 document.write('</p>');
